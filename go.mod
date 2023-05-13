e8e1de6457b4d9015d162a23836520e2756e4c35
Chandrakanth Nethi <chandrakanth.nethi@gmail.com>
2023-05-13 13:49:00 +0530

    feat: library functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChandrakanthNethi/stringUtils
+
+go 1.17
---
+++ stringUtils.go
@@ -0,0 +1,11 @@
+package stringUtils
+
+import "strings"
+
+func Upper(inputString string) string {
+	return strings.ToUpper(inputString)
+}
+
+func Lower(inputString string) string {
+	return strings.ToLower(inputString)
+}
